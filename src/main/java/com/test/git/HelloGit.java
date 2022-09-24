be724ca87a03505b3a483fbc01ec76ef66c016ac
issga <2570850096@qq.com>
2022-09-24 18:17:15 +0800

    third commit

--- src/main/java/com/test/git/HelloGit.java
+++ src/main/java/com/test/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("helloGit");
         System.out.println("hello World");
+        System.out.println("hello SG");
     }
 }
